ef4ec3147256f9cd5570e9650dd7d92091985a5d
Aleksandr <alex625051@users.noreply.github.com>
2022-07-06 20:25:02 +0300

    работает Хорошая таблица соответстсвтия масштабов

--- app2.py
+++ app2.py
@@ -11,6 +11,27 @@ import uuid
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+import random
+
+print("Starting server")
+# globals:
+clusters={}
+maxesOfH3Layers = {}
+maxValue=0
+renderAllHexs = True
+
+redGradient= [
+'f9eaee',
+'fed6df',
+'ffc1ce',
+'ffacba',
+'ff97a5',
+'ff818e',
+'ff6b76',
+'fe525c',
+'f83641',
+'ef0324',
+]
 
 
 ya_h3_zoom = {
@@ -21,7 +42,49 @@ ya_h3_zoom = {
     "3":[1], "4":[1], "5":[1,2], "6":[2,3], "7":[3,4], "8":[3,4], "9":[4,5], "10":[5,6], "11":[6,7], "12":[6,7],
     "13":[7,8], "14":[8,9], "15":[8,9], "16":[9,10], "17":[10,11], "18":[11,12], "19":[11,12], "20":[12], "21":[12]
 }
+
+ya_h3_zoom = {
+    "3":[1], "4":[1], "5":[1,2,3], "6":[2,3,4], "7":[3,4,5], "8":[3,4,5], "9":[4,5,6], "10":[5,6,7], "11":[6,7,8], "12":[6,7, 8],
+    "13":[7,8,9], "14":[7,8,9,10], "15":[7,8,9,10], "16":[7,8,9,10], "17":[7,8,9,10], "18":[7,8,9,10], "19":[7,8,9,10], "20":[7,8,9,10], "21":[7,8,9,10]
+}
 degree_1=111 #km
+
+def generataData():
+    dataSet = []
+    for i in range(0, 100000):
+        dataSet.append({
+            "lon": random.uniform(40, 60),
+            "lat": random.uniform(40, 60),
+            "worker_id": uuid.uuid4()
+        })
+    return dataSet
+
+
+def generateClusters(dataSet, H3_zooms):
+    clusters={}
+    global maxValue
+    global maxesOfH3Layers
+    for H3_zoom in H3_zooms:
+        maxesOfH3Layers[H3_zoom] = {}
+        clusters[H3_zoom]={}
+        for point in dataSet:
+            hxgn=h3.geo_to_h3(lat=point['lat'], lng=point['lon'], resolution=H3_zoom)
+            if not hxgn in clusters[H3_zoom]:
+                clusters[H3_zoom][hxgn] =1
+            else:
+                clusters[H3_zoom][hxgn] = clusters[H3_zoom][hxgn] + 1
+            if clusters[H3_zoom][hxgn] > maxValue: maxValue = clusters[H3_zoom][hxgn]
+            maxesOfH3Layers[H3_zoom]['max']=maxesOfH3Layers[H3_zoom].get('max',0)+1
+        maxesOfH3Layers[H3_zoom]['colorW'] = (len(redGradient)-1) / (maxesOfH3Layers[H3_zoom]['max'] or 1)
+
+
+    return clusters
+
+
+
+
+
+
 def crossdomain(origin=None, methods=None, headers=None,
                 max_age=21600, attach_to_all=True,
                 automatic_options=True):
@@ -74,11 +137,13 @@ def fromPixelsToTileNumber (x, y):
 
 
 def create_hexagons(geoJson, h3zoom, ya_zoom):
+    maxesOfH3Layer=0
     hexagons = list(h3.polyfill(geoJson, h3zoom))
     polygons=[]
     for hex in hexagons:
-        polygons.append( {"plgn":h3.h3_set_to_multi_polygon([hex], geo_json=False),"hex":hex,"h3zoom":h3zoom})
-    return polygons
+        polygons.append( {"plgn":h3.h3_set_to_multi_polygon([hex], geo_json=False),"hex": hex, "h3zoom": h3zoom})
+        maxesOfH3Layer = maxesOfH3Layer + clusters[h3zoom].get(hex,0)
+    return polygons, maxesOfH3Layer
 
 
 @app.route('/rom/<path:resp_string>')
@@ -110,12 +175,35 @@ def rom(resp_string):
     }
     polygons=[]
     for h3_zoom in ya_h3_zoom[ya_zoom]:
-        polygons = polygons + create_hexagons(geoJson, h3_zoom, ya_zoom=ya_zoom)
+        H3LayerPolygons, maxesOfH3Layer = create_hexagons(geoJson, h3_zoom, ya_zoom=ya_zoom)
+        polygons = polygons + H3LayerPolygons
 
 
 
     features=[]
+    # len_redGradient=len(redGradient)
+    # colorW=(len(redGradient)-1)/maxValue
     for polygon in polygons:
+        colorW = maxesOfH3Layers[h3_zoom]["colorW"]
+        strokeColor = "2222ff";
+        if not polygon['hex'] in clusters[polygon['h3zoom']]:
+            fill=False;
+            asses=0
+            color = "ffffff"
+            if not renderAllHexs:
+                continue
+        else:
+            asses = clusters[polygon['h3zoom']][polygon['hex']]
+            if h3_zoom == ya_h3_zoom[ya_zoom][-1]:
+                colorI = int(asses * colorW)
+                color= redGradient[colorI]
+                fill = True;
+            elif h3_zoom == ya_h3_zoom[ya_zoom][0]:
+                print("------------------------------------------------------------------")
+                colorI = int(asses * colorW)
+                strokeColor = redGradient[colorI]
+                fill = False;
+
         features.append({
       "type": "Feature",
       "id": polygon['hex'],
@@ -124,20 +212,20 @@ def rom(resp_string):
         "type": "Polygon"
       },
         "properties":{
-                "hintContent": polygon['hex'],
-                "balloonContent": polygon['hex']
+                # "hintContent": polygon['hex'],
+                "hintContent": f'ассайтов: {asses}',
+                "balloonContent": f'ассайтов: {asses}'
             },
             "options":{
-                "zIndex": 30+polygon['h3zoom']
+                "zIndex": 30+polygon['h3zoom'],
 
-                # "fillColor":"020202",
+                "fillColor": color,
                 # "opacity":0.1,
-                # "fill":  False,
-                # "strokeColor":"0101ff",
+                "fill":  fill,
+                "strokeColor": strokeColor,
                 # "strokeOpacity":1
             }
     })
-    print(len(features))
     fc2={
   "type": "FeatureCollection",
   "features": features
@@ -156,6 +244,15 @@ def rom(resp_string):
 def run_it():
     return send_file('index2.html')
 
+def main():
+    global clusters
+    dataSet=generataData()
+    clusters = generateClusters(dataSet, range(1,11))
 
 if __name__ == '__main__':
     app.run(threadaded = True)
+else:
+    main()
+
+
+
